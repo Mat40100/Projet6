c5625d2a03c82215c6592d7663c969fe82190578
root <root@vps581995.ovh.net>
2018-08-30 19:44:08 +0200

    resend to local ...

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,6 @@ $(document).ready(function () {
 
             $.ajax({
                 url : '/loadMore',
-                dataType : 'html',
                 type: 'GET',
                 data: {
                     tricksNumber: tricksNumber
